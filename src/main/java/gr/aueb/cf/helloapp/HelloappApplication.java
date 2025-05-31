6c8a4c682bfd25a5b320ae0593f58d3cf0fa8380
G. Panagopoulos <panagopoulosgevrgios@gmail.com>
2025-05-31 13:27:52 +0300

    first commit

---
+++ src/main/java/gr/aueb/cf/helloapp/HelloappApplication.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.helloapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloappApplication.class, args);
+	}
+
+}
